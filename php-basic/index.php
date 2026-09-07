57e208918a897786d9a646226d973d22fa570a29
名倉　由華 <yuru1322@gmail.com>
2026-09-07 15:28:36 +0900

    kadai

---
+++ php-basic/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="説明文">
+  <title>PHP基礎編</title>
+
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
+  <link rel="stylesheet" href="./css/style.css">
+  <script src="./js/script.js" defer></script>
+</head>
+<body>
+<p>この文章はHTMLで出力しています。</p>
+<p>
+  <?php
+  echo 'この文章はPHPで出力しています。';
+  ?>
+</p>
+  
+
+</body>
+</html>
---
+++ php-basic/variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+       // 変数に値を代入する
+        $user_name = '侍太郎';
+
+       // 変数の中身を出力する
+        echo $user_name;
+        ?>
+    </p>
+</body>
+
+</html>
