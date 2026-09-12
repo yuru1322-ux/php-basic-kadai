69001f47a7713399cc834d825c182b6b09b490f6
名倉　由華 <yuru1322@gmail.com>
2026-09-12 12:30:51 +0900

    追加

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -13,11 +13,11 @@
         $nums = [15, 4, 18, 23, 10 ];
         function sort_2way($array,$order) {
           if($order == true){
-            echo '昇順にソートします';
+            echo '昇順にソートします。';
             sort($array);
           }
           else{
-            echo '降順にソートします';
+            echo '降順にソートします。';
             rsort($array);
           };
           foreach($array as $value){
